f65ebb83b0498a6d49210afe3243d1ffcf76ca6c
Chuck <tengchenc@gmail.com>
2023-05-22 11:55:45 +0800

    chore: change formatter

--- request.py
+++ request.py
@@ -3,23 +3,24 @@
 import requests
 import json
 
+
 # common request func
 def doRequest(headers, params):
-    url = 'http://query.sse.com.cn/commonQuery.do'
-    params['jsonCallBack'] = 'jsonpCallback666'
+    url = "http://query.sse.com.cn/commonQuery.do"
+    params["jsonCallBack"] = "jsonpCallback666"
     response = requests.get(url, headers=headers, params=params)
-    response.encoding = 'utf-8'
+    response.encoding = "utf-8"
     # deal with jsonp response
     data = response.text
-    data = data.replace('jsonpCallback666(', '')
-    data = data.replace(')', '')
+    data = data.replace("jsonpCallback666(", "")
+    data = data.replace(")", "")
     # to json
     data = json.loads(data)
     # get `result` from dict object
-    if data['result'] is None:
-        print('no result')
+    if data["result"] is None:
+        print("no result")
         return
-    data = data['result']
+    data = data["result"]
     # printf data as json format
     # print(json.dumps(data, indent=4, ensure_ascii=False))
     return data
@@ -27,42 +28,36 @@ def doRequest(headers, params):
 
 def get_sse_bond_list():
     # set referer header
-    headers = {
-        'Referer': 'http://bond.sse.com.cn/'
-    }
+    headers = {"Referer": "http://bond.sse.com.cn/"}
     # set request params
     params = {
-        'isPagination': 'true',
-        'pageHelp.pageSize': '1',
-        'bond_type': '0',
+        "isPagination": "true",
+        "pageHelp.pageSize": "1",
+        "bond_type": "0",
         # 'status': '2',
-        'sqlId': 'COMMON_SSE_ZCZZQXMLB',
+        "sqlId": "COMMON_SSE_ZCZZQXMLB",
     }
     return doRequest(headers, params)
 
 
 def get_sse_bond_reference(audit_id):
-    headers = {
-        'Referer': 'http://bond.sse.com.cn/'
-    }
+    headers = {"Referer": "http://bond.sse.com.cn/"}
     params = {
-        'isPagination': 'false',
-        'sqlId': 'COMMON_SSE_ZCZZQXMXXXX_XXPLWJ_ZGSMS',
-        'audit_id': audit_id,
+        "isPagination": "false",
+        "sqlId": "COMMON_SSE_ZCZZQXMXXXX_XXPLWJ_ZGSMS",
+        "audit_id": audit_id,
     }
     return doRequest(headers, params)
 
 
 def get_sse_bond_feedback(audit_id):
     # set referer header
-    headers = {
-        'Referer': 'http://bond.sse.com.cn/'
-    }
+    headers = {"Referer": "http://bond.sse.com.cn/"}
     # set request params
     params = {
-        'isPagination': 'false',
-        'sqlId': 'COMMON_SSE_ZCZZQXMXXXX_FKXX_ALL',
-        'audit_id': audit_id,
+        "isPagination": "false",
+        "sqlId": "COMMON_SSE_ZCZZQXMXXXX_FKXX_ALL",
+        "audit_id": audit_id,
     }
 
     return doRequest(headers, params)
@@ -75,19 +70,23 @@ def get_bond_and_feedback():
     if list is None:
         return
     for bond in list:
-        audit_id = bond['BOND_NUM']
+        audit_id = bond["BOND_NUM"]
         feedback = get_sse_bond_feedback(audit_id)
         reference_files = get_sse_bond_reference(audit_id)
-       
-        result.append({
-            'bond': bond,
-            'feedback': feedback if feedback is not None else [],
-            'reference_file': reference_files[0] if reference_files is not None else []
-        })
+
+        result.append(
+            {
+                "bond": bond,
+                "feedback": feedback if feedback is not None else [],
+                "reference_file": reference_files[0]
+                if reference_files is not None
+                else [],
+            }
+        )
 
     print(json.dumps(result, indent=4, ensure_ascii=False))
     # save result as json file
-    with open('sse_bond.json', 'w', encoding='utf-8') as f:
+    with open("sse_bond.json", "w", encoding="utf-8") as f:
         f.write(json.dumps(result, indent=4, ensure_ascii=False))
 
 
