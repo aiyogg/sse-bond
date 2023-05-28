48468f4a23e4741b60de29e92e3b31d55ce87d5f
chuck <tengchenc@gmail.com>
2023-05-28 22:05:07 +0800

    refactor: logger

--- logger.py
+++ logger.py
@@ -1,9 +1,22 @@
 import logging
-import sys
 
 
-def configure_logging():
-    logging.basicConfig(level=logging.DEBUG)
-    console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setLevel(logging.DEBUG)
-    logging.getLogger().addHandler(console_handler)
+class Logger:
+    def __init__(self, log_file):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setLevel(logging.ERROR)
+        file_handler.setFormatter(formatter)
+        self.logger.addHandler(file_handler)
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
+        console_handler.setFormatter(formatter)
+        self.logger.addHandler(console_handler)
+
+    def log_error(self, message):
+        self.logger.error(message)
+
+    def log_info(self, message):
+        self.logger.info(message)
