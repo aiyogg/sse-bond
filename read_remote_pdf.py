224f4984a159e40f36633afce779fb12220d72eb
Chuck <tengchenc@gmail.com>
2023-05-30 17:41:19 +0800

    feat: replace some page numbers

--- read_remote_pdf.py
+++ read_remote_pdf.py
@@ -1,21 +1,13 @@
 """
 read the remote PDF content
 """
+import re
 import requests
-from logger import Logger
+from logger import logger
 from pypdf import PdfReader
-
-logger = Logger("error.log")
 
 
 def read_remote_pdf(pdf_url):
-    parts = []
-
-    def visitor_body(text, cm, tm, font_dict, font_size):
-        y = tm[5]
-        if y > 53:
-            parts.append(text.replace("\n", ""))
-
     try:
         response = requests.get(pdf_url)
         logger.log_info(response.status_code)
@@ -23,12 +15,16 @@ def read_remote_pdf(pdf_url):
             file.write(response.content)
         pdf_file = open("temp.pdf", "rb")
         pdf_reader = PdfReader(pdf_file)
-    except Exception as e:
-        logger.log_error(e)
-        return
-    page_count = pdf_reader.pages
-    for page in page_count:
-        page.extract_text(visitor_text=visitor_body)
 
-    text_body = "".join(parts)
+        page_count = pdf_reader.pages
+        text_body = ""
+        for page in page_count:
+            text_body += page.extract_text()
+    except Exception as e:
+        logger.log_error("read_remote_pdf Exception:", e)
+        return
+
+    pdf_file.close()
+
+    text_body = re.sub(r"\n|-\s?\d+\s?-", " ", text_body)
     return text_body
